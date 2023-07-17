ae7c005ba4f7fab49f7cff8aceccf4ffa8d221d0
Akib <akib7599@gmail.com>
2023-07-17 13:17:46 +0530

    create a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/navbar/navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <h1>all component</h1>
     </div>
   );
